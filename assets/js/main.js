6bb619fc292e397487f831509d0bddfe5ce177c8
James Anwyl <james.anwyl@MacBook-Pro.local>
2021-08-04 22:55:16 +0100

    Fix nav and hero issues caused by enabling strict mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,101 +1,99 @@
-!(function () {
-  "use strict";
-  // Navbar
-  var nav = document.querySelector('.nav');
-  var navItems = document.querySelector(".nav__list");
-  var navBurger = document.querySelector('.js-nav-burger')
-  var navOverlay = document.querySelector('.nav__overlay')
+"use strict";
+// Navbar
+var nav = document.querySelector('.nav');
+var navItems = document.querySelector(".nav__list");
+var navBurger = document.querySelector('.js-nav-burger')
+var navOverlay = document.querySelector('.nav__overlay')
 
-  // Unless we're at the top of page, use the sticky nav
-  function switchToStickyNav() {
-    nav.classList.add("nav__colored");
-    if (document.body.scrollTop === 0 && !navOverlay.classList.contains('active')) { 
-      nav.classList.remove("nav__colored")
-    }
+// Unless we're at the top of page, use the sticky nav
+function switchToStickyNav() {
+  nav.classList.add("nav__colored");
+  if (window.scrollY === 0 && !navOverlay.classList.contains('active')) { 
+    nav.classList.remove("nav__colored")
   }
-  window.onscroll = function () {
-    switchToStickyNav()
-  }
-  window.onload = function () {
-    switchToStickyNav()
-  }
+}
+window.onscroll = function () {
+  switchToStickyNav()
+}
+window.onload = function () {
+  switchToStickyNav()
+}
 
-  // Toggle burger menu 
-  navBurger.addEventListener('click', e => {
-    navItems.classList.toggle('d_flex')
-    navOverlay.classList.toggle('active')
-    nav.classList.add("nav__colored");
-  })
+// Toggle burger menu 
+navBurger.addEventListener('click', e => {
+  navItems.classList.toggle('d_flex')
+  navOverlay.classList.toggle('active')
+  nav.classList.add("nav__colored");
+})
 
-  // Carousel config
-  var swiper = new Swiper(".clientSwiper", {
-    spaceBetween: 15,
-    slidesPerView: "auto",
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+// Carousel config
+var swiper = new Swiper(".clientSwiper", {
+  spaceBetween: 15,
+  slidesPerView: "auto",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  // Open hero video in modal
-  MediaBox(".mediabox");
+// Open hero video in modal
+MediaBox(".mediabox");
 
-  // Attach newsletter form submission handler
-  window.addEventListener("load", function () {
-    document
-      .querySelector(".signup__form")
-      .addEventListener("submit", function (e) {
-        e.preventDefault();
-        submitNewsletterForm();
-      });
-  });
+// Attach newsletter form submission handler
+window.addEventListener("load", function () {
+  document
+    .querySelector(".signup__form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      submitNewsletterForm();
+    });
+});
 
-  // Send Mail
-  function submitNewsletterForm() {
-    var emailAddr = document.getElementById("email").value;
-    var errorMsg = document.querySelector(".signup__form_error");
-    var successMsg = document.querySelector(".signup__form_success");
-    var submitBtn = document.getElementById("newsletterSubmit");
-    var emailInput = document.getElementById("email");
-    var xhttp = new XMLHttpRequest();
+// Send Mail
+function submitNewsletterForm() {
+  var emailAddr = document.getElementById("email").value;
+  var errorMsg = document.querySelector(".signup__form_error");
+  var successMsg = document.querySelector(".signup__form_success");
+  var submitBtn = document.getElementById("newsletterSubmit");
+  var emailInput = document.getElementById("email");
+  var xhttp = new XMLHttpRequest();
 
-    xhttp.open(
-      "POST",
-      "https://frontend-trial-api.qa.parallax.dev/api/newsletter",
-      true
-    );
+  xhttp.open(
+    "POST",
+    "https://frontend-trial-api.qa.parallax.dev/api/newsletter",
+    true
+  );
 
-    xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.setRequestHeader("Content-type", "application/json");
 
-    xhttp.send(
-      JSON.stringify({
-        email: emailAddr,
-      })
-    );
+  xhttp.send(
+    JSON.stringify({
+      email: emailAddr,
+    })
+  );
 
-    xhttp.onload = function () {
-      var res = JSON.parse(this.responseText);
-      if (res.success) {
-        successMsg.innerHTML = "Thanks for signing up!";
-        errorMsg.classList.remove("d_block");
-        successMsg.classList.add("d_block");
-        submitBtn.disabled = true;
-        emailInput.disabled = true;
-      } else {
-        errorMsg.innerHTML = res.errors.email
-          ? res.errors.email
-          : "There was an unexpected failure";
-        successMsg.classList.remove("d_block");
-        errorMsg.classList.add("d_block");
-      }
-    };
-
-    xhttp.onerror = function (e) {
-      console.log(e.target.status);
-      errorMsg.innerHTML = "There was an issue submitting the form (check that the API is up!)";
+  xhttp.onload = function () {
+    var res = JSON.parse(this.responseText);
+    if (res.success) {
+      successMsg.innerHTML = "Thanks for signing up!";
+      errorMsg.classList.remove("d_block");
+      successMsg.classList.add("d_block");
+      submitBtn.disabled = true;
+      emailInput.disabled = true;
+    } else {
+      errorMsg.innerHTML = res.errors.email
+        ? res.errors.email
+        : "There was an unexpected failure";
       successMsg.classList.remove("d_block");
       errorMsg.classList.add("d_block");
-    };
-  }
-})();
+    }
+  };
+
+  xhttp.onerror = function (e) {
+    console.log(e.target.status);
+    errorMsg.innerHTML = "There was an issue submitting the form (check that the API is up!)";
+    successMsg.classList.remove("d_block");
+    errorMsg.classList.add("d_block");
+  };
+}
